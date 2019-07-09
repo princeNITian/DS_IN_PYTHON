5f3c7cb5b2d15df9f6ca6edc412c5ce76c085c28
Prince Raj <princeraj427@gmai.com>
2019-07-09 20:34:44 +0530

    Depth First Searh Added

---
+++ dfs.py
@@ -0,0 +1,33 @@
+
+class Node(object):
+
+    def __init__(self,name):
+        self.name = name;
+        self.adjacencyList = [];
+        self.visited = False;
+        self.predecessor = None;
+
+class DepthFirstSearch(object):
+    def dfs(self,node):
+        node.visited = True;
+        print("%s " % node.name);
+
+        for n in node.adjacencyList:
+            if not n.visited:
+                self.dfs(n);
+
+
+node1 = Node("A")
+node2 = Node("B")
+node3 = Node("C")
+node4 = Node("D")
+node5 = Node("E")
+
+
+node1.adjacencyList.append(node2)
+node1.adjacencyList.append(node3)
+node2.adjacencyList.append(node4)
+node4.adjacencyList.append(node5);
+
+dfs = DepthFirstSearch()
+dfs.dfs(node1)
